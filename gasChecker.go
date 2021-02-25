976fb98ea84c3c44041e6b5d0b05b826fb27fa06
Chad Tennent <stealthbos@protonmail.com>
2021-02-25 13:41:00 -0800

    .

--- gasChecker.go
+++ gasChecker.go
@@ -2,11 +2,10 @@ package main
 
 //TODO: Display gas price in Eth/USD
 //TODO: Experiment with other notification types - email, sms?
-//TODO: CLI frontend to take price threshhold parameters etc.
 import (
+	"flag"
 	"fmt"
 	"log"
-	"strings"
 	"time"
 
 	"github.com/go-toast/toast"
@@ -14,25 +13,39 @@ import (
 )
 
 func main() {
+	//oneEth := big.NewInt(1000000000000000000)
+	var userPrice = flag.Int("price", 0, "The price to check for (in gwei)")
+	flag.Parse()
 
-	averagePrice, err := gas.SuggestGasPrice(gas.GasPriorityAverage)
-	if err != nil {
-		log.Fatal(err)
-	}
-	priceString := strings.TrimRight(averagePrice.String(), "0")
-
+	fmt.Println(*userPrice)
 	notification := &toast.Notification{
-		AppID:   "GasChecker",
-		Title:   "Average gas price",
-		Message: fmt.Sprintf("The current average gas price is %s", priceString),
+		AppID: "GasChecker",
+		Title: "Average gas price",
+		Icon:  "C:/dev/go/gaschecker/icon.png",
 	}
 
-	for {
-		if priceString >= "100" {
-			if err := notification.Push(); err != nil {
+	prices := make(chan string)
+	go func() {
+		for {
+			averagePrice, err := gas.SuggestGasPrice(gas.GasPriorityAverage)
+			if err != nil {
 				log.Fatal(err)
 			}
+			//p := oneEth.Div(oneEth, averagePrice)
+			prices <- averagePrice.String()
 			time.Sleep(10 * time.Second)
+
 		}
+	}()
+	for price := range prices {
+		notification.Message = fmt.Sprintf("The current average gas price is %s", price[:3]+" Gwei.")
+		fmt.Println(notification.Message)
+		if fmt.Sprint(*userPrice) >= price[:3] {
+			err := notification.Push()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
 	}
 }
